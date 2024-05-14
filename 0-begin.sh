886379a2d959adcda926befb88e05fad5a5bb5ba
David Martins de Matos <david.m.matos@gmail.com>
2024-05-14 18:00:25 +0100

    Updated system for EF.

--- 0-begin.sh
+++ 0-begin.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 cat<<___EOT___ > GLOBAL-REPORT.md
-# Compiladores 2023/2024, LEIC-A, Projecto: Daily Run (reference: Entrega Zero)
+# Compiladores 2023/2024, LEIC-A, Projecto: Daily Run (reference: Entrega Intermédia)
 
 Full information can be found on the [official Fénix page](https://fenix.tecnico.ulisboa.pt/disciplinas/Com3/2023-2024/2-semestre) ([Projecto](https://fenix.tecnico.ulisboa.pt/disciplinas/Com3/2023-2024/2-semestre/projecto)) or on the [Compilers wiki](https://web.tecnico.ulisboa.pt/~david.matos/w/pt/index.php/Compiladores) ([Projecto](https://web.tecnico.ulisboa.pt/~david.matos/w/pt/index.php/Compiladores/Projecto_de_Compiladores)).
 
--- 1-clone.sh
+++ 1-clone.sh
@@ -3,7 +3,7 @@
 rm -rf checked-out checked-out-ref
 mkdir -p checked-out
 
-for i in {000..012} {014..067} {069..094} {096..120} 122 124 {126..131} 138 {190..200}; do
+for i in {000..012} {014..041} {043..067} {069..094} {096..100} {102..120} 122 124 {126..131} 138 {190..200}; do
   (cd checked-out; git clone git@gitlab.rnl.tecnico.ulisboa.pt:leic-a-co24/prj/$i.git)
 done
 
@@ -17,8 +17,8 @@ done
 #(cd checked-out; rm -rf 000 [2-9]??)
 
 cp -a checked-out checked-out-ref
-for i in {000..012} {014..067} {069..094} {096..120} 122 124 {126..131} 138 {190..200}; do
-  (cd checked-out-ref/$i; git -c advice.detachedHead=false checkout `git rev-list -1 --date=iso --before="2024-04-29T17:10:00+00:00" master`)
+for i in {000..012} {014..041} {043..067} {069..094} {096..100} {102..120} 122 124 {126..131} 138 {190..200}; do
+  (cd checked-out-ref/$i; git -c advice.detachedHead=false checkout `git rev-list -1 --date=iso --before="2024-05-14T17:10:00+00:00" master`)
 done
 
 #-- clear histories
