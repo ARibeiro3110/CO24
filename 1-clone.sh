9127065c6130fcc2121d5cedb94b616604dd69fd
David Martins de Matos <david.m.matos@gmail.com>
2024-05-30 00:41:44 +0100

    Only test projects under evaluation.

--- 1-clone.sh
+++ 1-clone.sh
@@ -4,7 +4,7 @@ rm -rf checked-out checked-out-ref
 mkdir -p checked-out
 
 #for i in {000..012} {014..041} {043..067} {069..094} {096..100} {102..120} 122 124 {126..131} 138 {181..182} {190..200}; do
-for i in {000..012} {014..041} {043..067} {069..094} {096..100} {102..120} 122 124 {126..131} 138; do
+for i in {001..012} {014..041} {043..067} {069..094} {096..100} {102..120} 122 124 {126..131} 138; do
   (cd checked-out; git clone git@gitlab.rnl.tecnico.ulisboa.pt:leic-a-co24/prj/$i.git)
 done
 
@@ -19,7 +19,7 @@ done
 
 cp -a checked-out checked-out-ref
 #for i in {000..012} {014..041} {043..067} {069..094} {096..100} {102..120} 122 124 {126..131} 138 {181..182} {190..200}; do
-for i in {000..012} {014..041} {043..067} {069..094} {096..100} {102..120} 122 124 {126..131} 138; do
+for i in {001..012} {014..041} {043..067} {069..094} {096..100} {102..120} 122 124 {126..131} 138; do
   (cd checked-out-ref/$i; git -c advice.detachedHead=false checkout `git rev-list -1 --date=iso --before="2024-05-14T17:10:00+00:00" master`)
 done
 
